2fb274c983339ffaf4529de39bf844287687e1a5
Clament <clament.john.k@gmail.com>
2021-05-07 14:45:59 +0530

    feat: Mouse move lister

--- Mouse_Event/src/main/kotlin/main.kt
+++ Mouse_Event/src/main/kotlin/main.kt
@@ -1,42 +1,26 @@
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.sp
 
 fun main() = Window(title = "Compose for Desktop", size = IntSize(400, 400)) {
-    val count = remember { mutableStateOf(0) }
-    Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxWidth()) {
-        val text = remember { mutableStateOf("Click magenta box!") }
-        Column {
-            @OptIn(ExperimentalFoundationApi::class)
-            Box(
-                modifier = Modifier
-                    .background(Color.Magenta)
-                    .fillMaxWidth(0.7f)
-                    .fillMaxHeight(0.2f)
-                    .combinedClickable(
-                        onClick = {
-                            text.value = "Click! ${count.value++}"
-                        },
-                        onDoubleClick = {
-                            text.value = "Double click! ${count.value++}"
-                        },
-                        onLongClick = {
-                            text.value = "Long click! ${count.value++}"
-                        }
-                    )
+    var color = remember { mutableStateOf(Color(0, 0, 0)) }
+    Box(
+        modifier = Modifier
+            .wrapContentSize(Alignment.Center)
+            .fillMaxSize()
+            .background(color = color.value)
+            .pointerMoveFilter(
+                onMove = {
+                    color.value = Color(it.x.toInt() % 256, it.y.toInt() % 256, 0)
+                    false
+                }
             )
-            Text(text = text.value, fontSize = 40.sp)
-        }
-    }
+    )
 }
