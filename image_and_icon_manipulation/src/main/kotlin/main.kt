6fafe518af9d61d39be7c17d7445160578ce764e
Clament <clament.john.k@gmail.com>
2021-05-07 12:11:57 +0530

    feat: Drawing raw image data using native canvas
    	You may want to draw raw image data, in which case you can use Canvas and drawIntoCanvas
    
    	Here we get image of type BufferedImage from File()
    	Get the byte array of BufferedImage
    	    By getting Blue, green Red (RGB) + Alpha info
    	    from the pixel info - by shifting bits
                (shr in kotlin, << in C/C++)
    	And finally return a bitmap
    	    which is used by drawIntoCanvas

--- image_and_icon_manipulation/src/main/kotlin/main.kt
+++ image_and_icon_manipulation/src/main/kotlin/main.kt
@@ -1,24 +1,76 @@
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import org.jetbrains.skija.Image
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.nativeCanvas
+import org.jetbrains.skija.Bitmap
+import org.jetbrains.skija.ColorAlphaType
+import org.jetbrains.skija.IRect
+import org.jetbrains.skija.ImageInfo
+import java.awt.image.BufferedImage
 import java.io.File
+import javax.imageio.ImageIO
 
 fun main() {
     Window {
-        val image = remember { imageFromFile(File("/home/clament/Desktop/sample.png")) }
-        Image(
-            bitmap = image,
-            contentDescription = "Sample",
+        // remember the calculation
+        val bitmap = remember { bitmapFromByteArray() }
+        Canvas(
             modifier = Modifier.fillMaxSize()
-        )
+        ) {
+            drawIntoCanvas { canvas ->
+                canvas.nativeCanvas.drawBitmapRect(
+                    bitmap,
+                    IRect(0, 0, bitmap.getWidth(), bitmap.getHeight()).toRect()
+                )
+            }
+        }
     }
 }
 
-fun imageFromFile(file: File): ImageBitmap {
-    return Image.makeFromEncoded(file.readBytes()).asImageBitmap()
+fun bitmapFromByteArray(): Bitmap {
+    var image: BufferedImage? = null
+    try {
+        image = ImageIO.read(File("/home/clament/Desktop/sample.png"))
+    } catch (e: Exception) {
+        // image file does not exist
+    }
+
+    if (image == null) {
+        image = BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB)
+    }
+    val pixels = getBytes(image) // assume we only have raw pixels
+
+    // allocate and fill skija Bitmap
+    val bitmap = Bitmap()
+    bitmap.allocPixels(ImageInfo.makeS32(image.width, image.height, ColorAlphaType.PREMUL))
+    bitmap.installPixels(bitmap.getImageInfo(), pixels, (image.width * 4).toLong())
+
+    return bitmap
+}
+
+// creating byte array from BufferedImage
+private fun getBytes(image: BufferedImage): ByteArray {
+    val width = image.width
+    val height = image.height
+
+    val buffer = IntArray(width * height)
+    image.getRGB(0, 0, width, height, buffer, 0, width)
+
+    val pixels = ByteArray(width * height * 4)
+
+    var index = 0
+    for (y in 0 until height) {
+        for (x in 0 until width) {
+            val pixel = buffer[y * width + x]
+            pixels[index++] = ((pixel and 0xFF)).toByte() // Blue component
+            pixels[index++] = (((pixel shr 8) and 0xFF)).toByte() // Green component
+            pixels[index++] = (((pixel shr 16) and 0xFF)).toByte() // Red component
+            pixels[index++] = (((pixel shr 24) and 0xFF)).toByte() // Alpha component
+        }
+    }
+
+    return pixels
 }
