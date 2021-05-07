b98386fe122eb542fb41d0e0646b0cb8c3dea526
Clament <clament.john.k@gmail.com>
2021-05-07 12:00:30 +0530

    feat: Loading images from device storage
    	To create an `ImageBitmap` from a loaded image stored in the device memory you can use `org.jetbrains.skija.Image`
    	Load an image from my desktop directory

--- image_and_icon_manipulation/src/main/kotlin/main.kt
+++ image_and_icon_manipulation/src/main/kotlin/main.kt
@@ -1,15 +1,24 @@
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import org.jetbrains.skija.Image
+import java.io.File
 
 fun main() {
     Window {
+        val image = remember { imageFromFile(File("/home/clament/Desktop/sample.png")) }
         Image(
-            bitmap = imageResource("images/sample.png"), // ImageBitmap
+            bitmap = image,
             contentDescription = "Sample",
             modifier = Modifier.fillMaxSize()
         )
     }
+}
+
+fun imageFromFile(file: File): ImageBitmap {
+    return Image.makeFromEncoded(file.readBytes()).asImageBitmap()
 }
