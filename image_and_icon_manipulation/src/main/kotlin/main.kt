7435a0bdc453dada4838a9d2311dabb7693f63b4
Clament <clament.john.k@gmail.com>
2021-05-07 12:25:43 +0530

    feat: Loading SVG images

--- image_and_icon_manipulation/src/main/kotlin/main.kt
+++ image_and_icon_manipulation/src/main/kotlin/main.kt
@@ -1,49 +1,15 @@
-import androidx.compose.desktop.AppManager
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import org.jetbrains.skija.Image
-import java.awt.image.BufferedImage
-import java.io.ByteArrayOutputStream
-import java.io.File
-import javax.imageio.ImageIO
+import androidx.compose.ui.res.svgResource
 
 fun main() {
-    val image = getWindowIcon()
     Window {
-        val imageAsset = remember { asImageAsset(image) }
         Image(
-            bitmap = imageAsset,
-            contentDescription = "Icon",
+            painter = svgResource("images/idea-logo.svg"),
+            contentDescription = "Idea logo",
             modifier = Modifier.fillMaxSize()
         )
     }
-
-    AppManager.focusedWindow?.setIcon(image)
-}
-
-fun getWindowIcon(): BufferedImage {
-    var image: BufferedImage? = null
-    try {
-        image = ImageIO.read(File("/home/clament/Desktop/sample.png"))
-    } catch (e: Exception) {
-        // image file does not exist
-    }
-
-    if (image == null) {
-        image = BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB)
-    }
-
-    return image
-}
-
-fun asImageAsset(image: BufferedImage): ImageBitmap {
-    val baos = ByteArrayOutputStream()
-    ImageIO.write(image, "png", baos)
-
-    return Image.makeFromEncoded(baos.toByteArray()).asImageBitmap()
 }
