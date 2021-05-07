c1e99c66ac2546d27bc580ebf91ef34699ba4317
Clament <clament.john.k@gmail.com>
2021-05-07 14:52:49 +0530

    feat: mouse click - workaround for adv fns

--- Mouse_Event/src/main/kotlin/main.kt
+++ Mouse_Event/src/main/kotlin/main.kt
@@ -1,39 +1,46 @@
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.gestures.forEachGesture
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerMoveFilter
-import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.input.pointer.AwaitPointerEventScope
+import androidx.compose.ui.input.pointer.PointerEvent
+import androidx.compose.ui.input.pointer.changedToDown
+import androidx.compose.ui.input.pointer.consumeDownChange
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import java.awt.event.MouseEvent
 
 fun main() = Window(title = "Compose for Desktop", size = IntSize(400, 400)) {
-    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
-        repeat(10) { index ->
-            var active = remember { mutableStateOf(false) }
-            Text(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(color = if (active.value) Color.Green else Color.White)
-                    .pointerMoveFilter(
-                        onEnter = {
-                            active.value = true
-                            false
-                        },
-                        onExit = {
-                            active.value = false
-                            false
-                        }
-                    ),
-                fontSize = 30.sp,
-                fontStyle = if (active.value) FontStyle.Italic else FontStyle.Normal,
-                text = "Item $index"
-            )
-        }
+    var lastEvent by remember { mutableStateOf<MouseEvent?>(null) }
+    Column {
+        Text(
+            text = "Custom button",
+            modifier = Modifier.pointerInput(Unit) {
+                forEachGesture {
+                    awaitPointerEventScope {
+                        lastEvent = awaitEventFirstDown().also {
+                            it.changes.forEach { it.consumeDownChange() }
+                        }.mouseEvent
+                    }
+                }
+            }
+        )
+        Text("Mouse event: ${lastEvent?.paramString()}")
     }
+
+}
+
+private suspend fun AwaitPointerEventScope.awaitEventFirstDown(): PointerEvent {
+    var event: PointerEvent
+    do {
+        event = awaitPointerEvent()
+    } while (
+        !event.changes.all { it.changedToDown() }
+    )
+    return event
 }
