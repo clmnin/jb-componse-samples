74070eae28203eeba78ac13ceb48e05e44f397ef
Clament <clament.john.k@gmail.com>
2021-05-07 14:48:21 +0530

    feat: Mouse enter listner

--- Mouse_Event/src/main/kotlin/main.kt
+++ Mouse_Event/src/main/kotlin/main.kt
@@ -1,26 +1,39 @@
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerMoveFilter
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 fun main() = Window(title = "Compose for Desktop", size = IntSize(400, 400)) {
-    var color = remember { mutableStateOf(Color(0, 0, 0)) }
-    Box(
-        modifier = Modifier
-            .wrapContentSize(Alignment.Center)
-            .fillMaxSize()
-            .background(color = color.value)
-            .pointerMoveFilter(
-                onMove = {
-                    color.value = Color(it.x.toInt() % 256, it.y.toInt() % 256, 0)
-                    false
-                }
+    Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
+        repeat(10) { index ->
+            var active = remember { mutableStateOf(false) }
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = if (active.value) Color.Green else Color.White)
+                    .pointerMoveFilter(
+                        onEnter = {
+                            active.value = true
+                            false
+                        },
+                        onExit = {
+                            active.value = false
+                            false
+                        }
+                    ),
+                fontSize = 30.sp,
+                fontStyle = if (active.value) FontStyle.Italic else FontStyle.Normal,
+                text = "Item $index"
             )
-    )
+        }
+    }
 }
