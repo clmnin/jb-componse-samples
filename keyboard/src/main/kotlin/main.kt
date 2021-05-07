1bbc478ea47d4bf0459b2571a742d0e35da5763e
Clament <clament.john.k@gmail.com>
2021-05-07 14:38:20 +0530

    feat: keyboard - window-scoped events
    	AppWindow instances have a keyboard property. It is possible to use it to define keyboard shortcuts that are always active in the current window

--- keyboard/src/main/kotlin/main.kt
+++ keyboard/src/main/kotlin/main.kt
@@ -1,49 +1,36 @@
+import androidx.compose.desktop.AppWindow
 import androidx.compose.desktop.Window
+import androidx.compose.foundation.Text
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.TextField
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.key.*
+import androidx.compose.ui.input.key.ExperimentalKeyInput
+import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.input.key.shortcuts
 
+@OptIn(ExperimentalKeyInput::class)
 fun main() = Window(title = "Compose for Desktop", size = IntSize(300, 300)) {
     MaterialTheme {
-        // What is remember -
-        // it is used as a state. When the state changes it UI is recomposed
-        // remember is a function that gives another composable function memory.
-        // A value computed by `remember` is stored in the composition during initial composition, and that stored
-        // value is returned during recomposition. You can use remember to store both mutable and immutable objects.
-        var consumedText by remember { mutableStateOf(0) }
-        var text by remember { mutableStateOf("") }
         Column(Modifier.fillMaxSize(), Arrangement.spacedBy(5.dp)) {
-            Text("Consumed text: $consumedText")
-            TextField(
-                value = text,
-                onValueChange = { text = it },
-                // Add behavior to Compose UI elements.
-                // For example, backgrounds, padding and click event listeners decorate or add behavior to rows, text or buttons.
-                // Here
-                // We use the KeyEvent modifier shortcuts and add events using on
-                modifier = Modifier.shortcuts {
-                    on(Key.CtrlLeft + Key.Minus) {
-                        consumedText -= text.length
-                        text = ""
-                    }
-                    on(Key.CtrlLeft + Key.Equals) {
-                        consumedText += text.length
-                        text = ""
+            Button(
+                modifier = Modifier.padding(4.dp),
+                onClick = {
+                    AppWindow(size = IntSize(200, 200)).also {
+                        it.keyboard.setShortcut(Key.Escape) {
+                            it.close()
+                        }
+                    }.show {
+                        Text("I'm popup!")
                     }
                 }
-            )
+            ) {
+                Text("Open popup")
+            }
         }
     }
 }
