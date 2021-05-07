25cc1e4415034dff62d4a0e71544f4b9ba5646a1
Clament <clament.john.k@gmail.com>
2021-05-07 12:27:11 +0530

    feat: Loading XML vector images

--- image_and_icon_manipulation/src/main/kotlin/main.kt
+++ image_and_icon_manipulation/src/main/kotlin/main.kt
@@ -2,13 +2,13 @@ import androidx.compose.desktop.Window
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.svgResource
+import androidx.compose.ui.res.vectorXmlResource
 
 fun main() {
     Window {
         Image(
-            painter = svgResource("images/idea-logo.svg"),
-            contentDescription = "Idea logo",
+            imageVector = vectorXmlResource("images/compose-logo.xml"),
+            contentDescription = "Compose logo",
             modifier = Modifier.fillMaxSize()
         )
     }
